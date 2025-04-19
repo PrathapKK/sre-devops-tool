1471dbb240664bb8e25b8a1aaa15cfa5140887c1
prathap <prathapkk@gmail.com>
2025-04-18 19:33:44 -0500

    CRUD for any kind of entity with generic routes,schemas

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    """Base configuration"""
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sredbtooluser:libPwdadmin%40512@localhost:3306/sredb'
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
